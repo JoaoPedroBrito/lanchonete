807b2640d81363c4194cb67d31ea08bd9aa7ae0e
JoaoPedroBrito <jp.brito_leop@hotmail.com>
2022-11-08 21:45:13 -0300

    commit

--- src/componentes/home.tsx
+++ src/componentes/home.tsx
@@ -1,20 +1,75 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, FlatList, Pressable, Alert } from "react-native";
+import { Produto } from "../entidades/produto";
 
 interface ItemLista {
-    nome: string;
+    produto: Produto;
 }
 
-const Item = ({ nome }: ItemLista) => {
+const Item = ({ produto }: ItemLista) => {
     return (
         <View>
-            <Text>Nome: {nome}</Text>
+            <Text>Nome: {produto.nome}</Text>
+            <Text>Valor: {produto.valor}</Text>
+            <Pressable
+                onPress={() => {
+                    Alert.alert(`codigo: ${produto.codigo}`);
+                }}
+            ><Text>Click</Text></Pressable>
+        </View>
+    )
+}
+
+
+function fnRenderItem({ item }: any) {
+    return (
+        <View>
+            <Item produto={item}></Item>
+            <Text>----------------------</Text>
         </View>
     )
 }
 
 const Home = () => {
-    return (<Item nome="Joao"></Item>);
+    const produto_1 = {
+        codigo: 1,
+        nome: "Hot-Dog",
+        descricao: "Pão e salsicha",
+        valor: 7.50,
+        categoria: {
+            codigo: 1,
+            descricao: "Lanche"
+        }
+    };
+
+    const produto_2 = {
+        codigo: 2,
+        nome: "Hot-Dogão",
+        descricao: "Pão e salsicha e salsicha",
+        valor: 10.50,
+        categoria: {
+            codigo: 1,
+            descricao: "Lanche"
+        }
+    };
+
+    const produtos = [
+        produto_1,
+        produto_2
+    ];
+
+    //return (<Item produto={produto_1}></Item>);
+    return (
+        <View>
+            <Text style={{ textAlign: "center" }}>
+                Lista de Produto
+            </Text>
+            <FlatList
+                data={produtos}
+                renderItem={fnRenderItem}
+            />
+        </View>
+    );
 }
 
 export default Home;
